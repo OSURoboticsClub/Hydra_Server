d892cc061c81cd0c6acc83985204f8f50b1af8d0
Tomas McMonigal <mcmonigt@oregonstate.edu>
2020-02-27 15:21:10 -0800

    multi-threaded working w/ latitude and longitude

--- hydra_server.py
+++ hydra_server.py
@@ -10,53 +10,61 @@
 import asyncio
 from mavsdk import System
 
-async def run():
-    # Init the drone
-    drone = System()
-    await drone.connect(system_address="udp://:14555")
-
-    # Start the tasks
-    asyncio.ensure_future(print_battery(drone))
-    asyncio.ensure_future(print_gps_info(drone))
-    asyncio.ensure_future(print_in_air(drone))
-    asyncio.ensure_future(print_position(drone))
-
-async def print_battery(drone):
-    async for battery in drone.telemetry.battery():
-        print(f"Battery: {battery.remaining_percent}")
-
-
-async def print_gps_info(drone):
-    async for gps_info in drone.telemetry.gps_info():
-        print(f"GPS info: {gps_info}")
-
-
-async def print_in_air(drone):
-    async for in_air in drone.telemetry.in_air():
-        print(f"In air: {in_air}")
-
-
-async def print_position(drone):
-    async for position in drone.telemetry.position():
-        print(position)
+# async def run():
+#     # Init the drone
+#     drone = System()
+#     await drone.connect(system_address="udp://:14555")
+#
+#     # Start the tasks
+#     asyncio.ensure_future(print_battery(drone))
+#     asyncio.ensure_future(print_gps_info(drone))
+#     asyncio.ensure_future(print_in_air(drone))
+#     asyncio.ensure_future(print_position(drone))
+#
+# async def print_battery(drone):
+#     async for battery in drone.telemetry.battery():
+#         print(f"Battery: {battery.remaining_percent}")
+#
+#
+# async def print_gps_info(drone):
+#     async for gps_info in drone.telemetry.gps_info():
+#         print(f"GPS info: {gps_info}")
+#
+#
+# async def print_in_air(drone):
+#     async for in_air in drone.telemetry.in_air():
+#         print(f"In air: {in_air}")
+#
+#
+# async def print_position(drone):
+#     async for position in drone.telemetry.position():
+#         print(position)
 
 async def echo_server(reader, writer):
     while True:
         drone = System()
         await drone.connect(system_address="udp://:14555")
-        print("hi")
-        data = await reader.read(100)  # Max number of bytes to read
-        if not data:
-            break
         async for position in drone.telemetry.position():
-            # data_out = b''
-            # print(position.latitude_deg)
-            tmp = str(position.latitude_deg)
-            print(tmp)
-            # data_out = str(position.latitude_deg.encode('utf-8'))
-            writer.write(tmp.encode())
-            await writer.drain()  # Flow control, see later
-            # print("end")
+            print("waiting on data requests")
+            data = await reader.read(100)  # Max number of bytes to read
+            data_in = data.decode() # decodes utf-8 only
+            data_in = data_in.replace('\r\n', '')
+            if data_in == "latitude":
+                print("latitude requested")
+                data_out = str(position.latitude_deg)
+                data_out = data_out + '\n'
+                writer.write(data_out.encode())
+                await writer.drain()
+            elif data_in == "longitude":
+                print("longitude requested")
+                data_out = str(position.longitude_deg)
+                data_out = data_out + '\n'
+                writer.write(data_out.encode())
+                await writer.drain()
+            else:
+                print("data requested not supported")
+                output_string = '|' + data_in + '|'
+                print(output_string)
     writer.close()
 
 async def main(host, port):
