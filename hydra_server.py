51f2c7af0e15f758b0b4cc566ae0e4950a1b9f8c
Tomas McMonigal <mcmonigt@oregonstate.edu>
2020-03-07 18:31:01 -0800

    multi-threaded functionality working with no known issues

--- hydra_server.py
+++ hydra_server.py
@@ -9,66 +9,97 @@
 """
 import asyncio
 from mavsdk import System
+from concurrent.futures import TimeoutError
 
-# async def run():
-#     # Init the drone
-#     drone = System()
-#     await drone.connect(system_address="udp://:14555")
-#
-#     # Start the tasks
-#     asyncio.ensure_future(print_battery(drone))
-#     asyncio.ensure_future(print_gps_info(drone))
-#     asyncio.ensure_future(print_in_air(drone))
-#     asyncio.ensure_future(print_position(drone))
-#
-# async def print_battery(drone):
-#     async for battery in drone.telemetry.battery():
-#         print(f"Battery: {battery.remaining_percent}")
-#
-#
-# async def print_gps_info(drone):
-#     async for gps_info in drone.telemetry.gps_info():
-#         print(f"GPS info: {gps_info}")
-#
-#
-# async def print_in_air(drone):
-#     async for in_air in drone.telemetry.in_air():
-#         print(f"In air: {in_air}")
-#
-#
-# async def print_position(drone):
-#     async for position in drone.telemetry.position():
-#         print(position)
+async def echo_server(reader, writer, drone):
 
-async def echo_server(reader, writer):
     while True:
-        drone = System()
-        await drone.connect(system_address="udp://:14555")
-        async for position in drone.telemetry.position():
             print("waiting on data requests")
-            data = await reader.read(100)  # Max number of bytes to read
+            marker = 1
+
+            try:
+                data = await asyncio.wait_for(reader.read(100), timeout=3)
+                if not data: # client has disconnected
+                    print("connection closed by client: closing connection with client")
+                    break
+            except TimeoutError:
+                print("Timeout error: closing connection with client")
+                break
+
+            # ******** if data is valid and connection active ****************
             data_in = data.decode() # decodes utf-8 only
             data_in = data_in.replace('\r\n', '')
+
             if data_in == "latitude":
                 print("latitude requested")
-                data_out = str(position.latitude_deg)
-                data_out = data_out + '\n'
-                writer.write(data_out.encode())
-                await writer.drain()
+                async for position in drone.telemetry.position():
+                    data_out = str(position.latitude_deg) + '\n'
+                    # data_out = data_out + '\n'
+                    writer.write(data_out.encode())
+                    await writer.drain()
+                    break
+
+            elif data_in == "altitude":
+                async for position in drone.telemetry.position():
+                    print("altitude requested")
+                    data_out = str(position.relative_altitude_m) + '\n'
+                    writer.write(data_out.encode())
+                    await writer.drain()
+                    break
+
             elif data_in == "longitude":
-                print("longitude requested")
-                data_out = str(position.longitude_deg)
-                data_out = data_out + '\n'
-                writer.write(data_out.encode())
-                await writer.drain()
+                async for position in drone.telemetry.position():
+                    print("longitude requested")
+                    data_out = str(position.longitude_deg) + '\n'
+                    # data_out = data_out + '\n'
+                    writer.write(data_out.encode())
+                    await writer.drain()
+                    break
+
+            elif data_in == "battery":
+                async for battery in drone.telemetry.battery():
+                    print("battery percentage requested")
+                    data_out = str(battery.voltage_v) + '\n'
+                    writer.write(data_out.encode())
+                    await writer.drain()
+                    break
+
+            elif data_in == "arm":
+                print("-- Arming")
+                await drone.action.arm()
+
+            elif data_in == "disarm":
+                print("-- Disarming")
+                await drone.action.disarm()
+
+            elif data_in == "kill":
+                print("-- Killing")
+                await drone.action.kill()
+
             else:
-                print("data requested not supported")
+                print("error: data requested not supported, closing connection")
                 output_string = '|' + data_in + '|'
                 print(output_string)
     writer.close()
 
 async def main(host, port):
-        server = await asyncio.start_server(echo_server, host, port)
+    drone = System()
+    await drone.connect(system_address="udp://:14555")
+    print("Waiting for drone to connect...")
+    async for state in drone.core.connection_state():
+        if state.is_connected:
+            print(f"Drone discovered with UUID: {state.uuid}")
+            break
+
+    print("Waiting for drone to have a global position estimate...")
+    async for health in drone.telemetry.health():
+        if health.is_global_position_ok:
+            print("Global position estimate ok")
+            break
+
+    print("Ready to accept incoming connections")
+    server = await asyncio.start_server(lambda r, w: echo_server(r, w, drone), host, port)
+    async with server:
         await server.serve_forever()
 
 asyncio.run(main('127.0.0.1', 5000))
